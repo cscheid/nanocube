cc7e139be6688fd18930a091ed99e58b34884d06
Zhe Wang <zwang.contact@gmail.com>
2016-02-17 01:02:31 -0700

    test 10 dimensions

--- data/gen_test_data.py
+++ data/gen_test_data.py
@@ -12,7 +12,15 @@ valname: test_category 1 CATEGORY_B
 metadata: tbin 2016-01-01_00:00:00_3600s
 field: time nc_dim_time_2
 field: count1 nc_var_float_8
-field: count2 nc_var_float_8\n
+field: count2 nc_var_float_8
+field: count3 nc_var_float_8
+field: count4 nc_var_float_8
+field: count5 nc_var_float_8
+field: count6 nc_var_float_8
+field: count7 nc_var_float_8
+field: count8 nc_var_float_8
+field: count9 nc_var_float_8
+field: count10 nc_var_float_8\n
 """)
 
 for i in xrange(10001):
@@ -20,5 +28,5 @@ for i in xrange(10001):
         x = random.normalvariate(0, 10000)
     else:
         x = random.normalvariate(10000, 10000)
-    v = struct.pack('<iiBHdd', int(x + (1 << 24)), 1 << 24, i % 2, int(i ** 0.5), 1.3, 1.5)
+    v = struct.pack('<iiBHdddddddddd', int(x + (1 << 24)), 1 << 24, i % 2, int(i ** 0.5), 1.3, 1.5, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6,1.7,1.8)
     sys.stdout.write(v)
